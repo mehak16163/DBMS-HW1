4ce47003ad929b4afe45fb439147904bba773a56
Mehak Gupta <mehak16163@iiitd.ac.in>
2018-01-11 18:23:54 +0530

    part a is done

--- program_a.py
+++ program_a.py
@@ -1,3 +1,4 @@
+import sys
 file_name = input("Enter file name ")
 table = open(file_name,"r",1)
 print("Name of the file: ", table.name)
@@ -10,6 +11,7 @@ j=0;
 while j<num:
     print(fields[j])
     j=j+1
+j=0;
 print("Select one of the two options:\n1. Print entire content\n2. Find sum of a field")
 option = int(input("The option is: "))
 if option == 1:
@@ -23,4 +25,28 @@ if option == 1:
             k=k+1
         print("")
         i=i+1
+else:
+    print("Select the field")
+    f = input("")
+    f=f.lower()
+    j=0
+    while j<num:
+        if f == fields[j].lower().lstrip().rstrip():
+            break
+        j=j+1
+    if j==num:
+        print("No such field exists")
+        sys.exit("")
+    check = File_content[1].split(',')[j]
+    try:
+        val = float(check)
+    except ValueError:
+        print("This field is not numeric")
+        sys.exit("")
+    i=1
+    result=0
+    while i<length:
+        result = result + float(File_content[i].split(',')[j])
+        i=i+1;
+    print("The sum is " , result)
 table.close()
