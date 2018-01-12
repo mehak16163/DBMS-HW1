6ddece04a986212ec63cdf0e1d10e1efa656889e
Shravika Mittal <shravika16093@iiitd.ac.in>
2018-01-12 16:59:18 +0530

    calculating the efficiency - TestData

---
+++ testcase.py
@@ -0,0 +1,6 @@
+with open("TestFile.txt", "w") as file:
+    file.write("Name, Place, Mobile, Distance" + "\n")
+    for i in range (0, 12001):
+        file.write("Shravika Mittal     ,Noida               ,5232458250,24" + "\n")
+               
+    
